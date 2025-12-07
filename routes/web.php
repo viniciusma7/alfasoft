6df46a4c819fea7ef291c66b21f0529647f8ee97
Vinicius <vinicius.almeida@petroeng.com.br>
2025-12-07 14:39:48 -0300

    feat: redirect home route to contacts index

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', fn () => to_route('contacts.index'));
 
 Route::get('/dashboard', function () {
     return view('dashboard');
